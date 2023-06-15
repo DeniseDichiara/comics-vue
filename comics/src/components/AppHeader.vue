b13376dc634b303f23de641ece271ad494dc7482
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-15 22:50:46 +0200

    create

---
+++ comics/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>App footer</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'AppHeader',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
