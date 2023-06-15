47abc66ae5a66a775796f9e6248c10a941d683a8
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-16 01:52:26 +0200

    footer

--- comics/src/components/AppFooter.vue
+++ comics/src/components/AppFooter.vue
@@ -11,9 +11,43 @@
                 <p>News</p>
             </div>
         </h3>
-        <h3>DC</h3>
-        <h3>SITE</h3>
+        <h3>DC
+            <div class="bottom">
+                <p>Terms of Use</p>
+                <p>Comics</p>
+                <p>Movies</p>
+                <p>TV</p>
+                <p>Games</p>
+                <p>Videos</p>
+                <p>News</p>
+                <p>Games</p>
+                <p>Videos</p>
+                <p>News</p>
+            </div>
+
+        </h3>
+        <h3>SITE
+            <div class="bottom">
+                <p>Characters</p>
+                <p>Comics</p>
+                <p>Movies</p>
+                <p>TV</p>
+            </div>
+        </h3>
+
+        
     </footer>
+
+    <div class="lower-footer">
+            <div class="sign-up">
+                <h3>SIGN-UP NOW!</h3>
+            </div>
+            <div>
+                <h3>FOLLOW US</h3>
+
+            </div>
+
+        </div>
 </template>
 
 <script>
@@ -30,7 +64,7 @@ export default {
 <style>
 footer {
     background-color: #2b2b2b;
-    height: 30rem;
+    height: 20rem;
     display: flex;
     flex-direction: row;
     color: white;
@@ -51,4 +85,18 @@ p{
 .bottom{
     padding-top: 1rem;
 }
+
+.lower-footer{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding-bottom: 3rem;
+    padding-right: 10rem;
+    height: 10rem;
+    background-color: #303030;
+    color: white;
+}
+
+
 </style>
