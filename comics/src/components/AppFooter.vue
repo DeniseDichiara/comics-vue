ba61647e50a577c3f5f31db8c925aae31abdd5d8
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-16 01:31:46 +0200

    added elemts to the footer

--- comics/src/components/AppFooter.vue
+++ comics/src/components/AppFooter.vue
@@ -1,20 +1,54 @@
 <template>
-    <div>
-        <h1>App Footer</h1>
-    </div>
+    <footer>
+        <h3>DC COMICS
+            <div class="bottom">
+                <p>Characters</p>
+                <p>Comics</p>
+                <p>Movies</p>
+                <p>TV</p>
+                <p>Games</p>
+                <p>Videos</p>
+                <p>News</p>
+            </div>
+        </h3>
+        <h3>DC</h3>
+        <h3>SITE</h3>
+    </footer>
 </template>
 
 <script>
 export default {
-    name:'AppFooter',
-    data(){
-        return{
+    name: 'AppFooter',
+    data() {
+        return {
 
         }
     }
 }
 </script>
 
-<style lang="">
-    
+<style>
+footer {
+    background-color: #2b2b2b;
+    height: 30rem;
+    display: flex;
+    flex-direction: row;
+    color: white;
+}
+
+h3 {
+    padding-right: 2rem;
+    padding-top: 2rem;
+    padding-left: 10rem;
+}
+
+p{
+    font-size: small;
+    color: #959595;
+    padding-bottom: .5rem;
+}
+
+.bottom{
+    padding-top: 1rem;
+}
 </style>
