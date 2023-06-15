3360cf4f69a81cb488f8fba245e3775c0acdd1f5
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-15 22:51:09 +0200

    created App Footer

---
+++ comics/src/components/AppFooter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>App Footer</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'AppFooter',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
